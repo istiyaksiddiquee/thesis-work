fbf64211eb4875bf7745e59ce094b1e7f6dcbb5d
Istiyak H. Siddiquee <istiyaksiddiquee@gmail.com>
2024-10-24 16:24:56 +0200

    ready for RQ2Final2

--- datasets-for-exp/script.py
+++ datasets-for-exp/script.py
@@ -36,7 +36,7 @@ from sklearn.model_selection import RepeatedKFold, KFold, GridSearchCV
 # 2. check notebook for normalization column and selected column
 # 3. check codebase related settings
 
-ds = 1
+ds = 2
 trial = False
 feature_selection = 1
 wandb_project = "RQ2Final2"
