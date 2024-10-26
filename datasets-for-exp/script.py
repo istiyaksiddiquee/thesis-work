364cc01181f152dffc3bda7b68996b52ab2ecfb1
Istiyak H. Siddiquee <istiyaksiddiquee@gmail.com>
2024-10-26 23:08:05 +0200

    ready for RQ2Comb4

--- datasets-for-exp/script.py
+++ datasets-for-exp/script.py
@@ -40,9 +40,9 @@ from sklearn.model_selection import StratifiedKFold, HalvingGridSearchCV
 ds = 1
 trial = False
 quant = True
-feature_selection = 1
+feature_selection = 0
 data_imputation = 0
-wandb_project = "RQ2Comb3"
+wandb_project = "RQ2Comb4"
 folder_path = "RQ2Final1"
 
 removed_columns = ['size', 'max_breadth']
