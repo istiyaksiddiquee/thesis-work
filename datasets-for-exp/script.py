65513270b9b67fa0723a0842f7d44f39e66b6f7c
Istiyak H. Siddiquee <istiyaksiddiquee@gmail.com>
2024-10-28 07:30:04 +0100

    ready for rerun of RQ2Comb8 on PC11

--- datasets-for-exp/script.py
+++ datasets-for-exp/script.py
@@ -40,9 +40,9 @@ from sklearn.model_selection import StratifiedKFold, HalvingGridSearchCV
 ds = 2
 trial = False
 quant = True
-feature_selection = 1
+feature_selection = 0
 data_imputation = 0
-wandb_project = "RQ2Comb7"
+wandb_project = "RQ2Comb8"
 folder_path = "RQ2Final2"
 
 # [depth, size, max_breadth, virality, density, layer_ratio, structural_heterogeneity, characteristic_distance]
