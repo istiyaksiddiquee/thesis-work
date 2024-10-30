13ca193bb11bcfec7474042619e8a7f6119d9a29
Istiyak H. Siddiquee <istiyaksiddiquee@gmail.com>
2024-10-30 11:57:29 +0100

    ready for rerun of RQ3Final3 on PC8

--- datasets-for-exp/script.py
+++ datasets-for-exp/script.py
@@ -38,7 +38,7 @@ from sklearn.model_selection import StratifiedKFold, HalvingGridSearchCV
 # 3. check codebase related settings
 
 trial = False
-wandb_project = "RQ3Final2"
+wandb_project = "RQ3Final3"
 folder_path = wandb_project # change it for RQ2Final1, RQ2Final2
 data_imputation = 1
 feature_selection = 1
